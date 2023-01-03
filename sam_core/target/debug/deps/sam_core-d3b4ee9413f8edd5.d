b2856b1b994f456259f7967a1fe778714eb76f8f
thewoodfish <jasonholt2002@gmail.com>
2023-01-03 16:15:49 +0100

    package setup

--- sam_core/target/debug/deps/sam_core-d3b4ee9413f8edd5.d
+++
@@ -1,5 +0,0 @@
-/Users/explorer/Public/samaritan-core/sam_core/target/debug/deps/sam_core-d3b4ee9413f8edd5: src/main.rs
-
-/Users/explorer/Public/samaritan-core/sam_core/target/debug/deps/sam_core-d3b4ee9413f8edd5.d: src/main.rs
-
-src/main.rs:
--- sam_core/target/debug/sam_core.d
+++
@@ -1,1 +0,0 @@
-/Users/explorer/Public/samaritan-core/sam_core/target/debug/sam_core: /Users/explorer/Public/samaritan-core/sam_core/src/main.rs
